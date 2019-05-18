46a22def955bbdb7401360bd1e51f3518e2ff581
Austin-Src <48729572+Austin-Src@users.noreply.github.com>
2019-05-18 17:38:15 -0400

    Update tor.ps1

--- tor.ps1
+++ tor.ps1
@@ -1,6 +1,17 @@
 # Pull down and store current TOR node IP's
 [Net.ServicePointManager]::SecurityProtocol = "Tls12, Tls11, Tls, Ssl3"
-Invoke-WebRequest -Uri https://www.dan.me.uk/torlist/ -OutFile nodes.txt
+Invoke-WebRequest -Uri https://www.dan.me.uk/torlist/ -OutFile C:\PATH-TO-DOWNLOAD\nodes.txt
+
+# Get new data for rules
+$lines = 0
+$lines = gc C:\PATH-TO-DOWNLOAD\nodes.txt | Measure-Object -Line | Select-Object -expand Lines
+$iterations = (($lines-$lines%1000)/1000) + 1
+
+# Quit the program if no data was pulled down
+if($lines -lt 5) { 
+	Write-Host Server limited to one request every 30 minutes
+	exit 
+}
 
 # Delete all created TorBlock rules
 # Dumps all rules and parses for "TorBlock" and "TorBlocker
@@ -13,11 +24,8 @@ for ($t = 0; $t -lt $total/2; $t++) {
 	Remove-NetFireWallRule -DisplayName $name2
 }
 
-# Add New Firewall rules banning TOR IP's by the 1000's
+# Set New Firewall rules banning TOR IP's by the 1000's
 # (Maximum number of IP's allowed per rule is 1000)
-$lines = gc nodes.txt | Measure-Object -Line | Select-Object -expand Lines
-$iterations = (($lines-$lines%1000)/1000) + 1
-
 for($i = 0; $i -lt $iterations; $i++) {
 	$skip = $i * 1000
 	$ips = gc nodes.txt | Select -First 1000 -Skip $skip
